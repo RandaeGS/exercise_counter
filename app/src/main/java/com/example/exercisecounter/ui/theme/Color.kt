d65652c4f54c7805f02afd99cb0949e740a9b727
RandaeGS <garciasanchezrd@gmail.com>
2025-10-19 19:49:26 -0400

    add colors and custom fonts to theme

--- app/src/main/java/com/example/exercisecounter/ui/theme/Color.kt
+++ app/src/main/java/com/example/exercisecounter/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val dark_primary = Color(0xFF234C6A)
+val dark_secondary = Color(0xFF456882)
+val dark_tertiary = Color(0xFFD2C1B6)
+val dark_background = Color(0xFF1B3C53)
--- app/src/main/java/com/example/exercisecounter/ui/theme/Theme.kt
+++ app/src/main/java/com/example/exercisecounter/ui/theme/Theme.kt
@@ -12,32 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = dark_primary,
+    secondary = dark_secondary,
+    tertiary = dark_tertiary,
+    background = dark_background,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = Pink40,
 )
 
 @Composable
 fun ExerciseCounterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
